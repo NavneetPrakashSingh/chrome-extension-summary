68d54d4112fc03d12545d962ed363b100470f2ba
NavneetPrakashSingh <navneet_prakash_singh@live.com>
2023-05-15 16:31:42 -0700

    WIP changes

--- foreground.js
+++ foreground.js
@@ -1,60 +1,54 @@
 const url = window.location.href;
+async function main(){
+    let summary = await getSummary()
+    console.log(summary)
 
-console.log('-----------------------------')
-console.log(getSummary())
-console.log('-----------------------------')
+        
+    const ce_main_container = document.createElement('DIV');
+    const ce_name = document.createElement('DIV');
+    const ce_input = document.createElement('INPUT');
+    const ce_button = document.createElement('DIV');
 
-const ce_main_container = document.createElement('DIV');
-const ce_name = document.createElement('DIV');
-const ce_input = document.createElement('INPUT');
-const ce_button = document.createElement('DIV');
+    ce_main_container.classList.add('ce_main');
+    ce_name.id = 'ce_name';
 
-ce_main_container.classList.add('ce_main');
-ce_name.id = 'ce_name';
-ce_input.id = 'ce_input';
-ce_button.id = 'ce_button';
-
-ce_name.innerHTML = `Hello NAME`;
-ce_button.innerHTML = `Change name.`;
-
-ce_main_container.appendChild(ce_name);
-ce_main_container.appendChild(ce_input);
-ce_main_container.appendChild(ce_button);
+    ce_main_container.appendChild(ce_name);
 
 
-document.querySelector('body').appendChild(ce_main_container);
+    document.querySelector('body').appendChild(ce_main_container);
 
-chrome.runtime.sendMessage({ 
-    message: "get_name"
-}, response => {
-    if (response.message === 'success') {
-        ce_name.innerHTML = `Url is `+ window.location.href;
-    }
-});
-
-ce_button.addEventListener('click', () => {
     chrome.runtime.sendMessage({ 
-        message: "change_name",
-        payload: ce_input.value
+        message: "get_name"
     }, response => {
         if (response.message === 'success') {
-            ce_name.innerHTML = `Hello ${ce_input.value}`;
+            ce_name.innerHTML = `Summary is `+ summary["html"];
         }
     });
-});
+
+    ce_button.addEventListener('click', () => {
+        chrome.runtime.sendMessage({ 
+            message: "change_name",
+            payload: ce_input.value
+        }, response => {
+            if (response.message === 'success') {
+                ce_name.innerHTML = `Hello ${ce_input.value}`;
+            }
+        });
+    });
+}
+
+main()
+
 
 async function getSummary(){
     const data = new FormData();
 data.append("url", "https://techcrunch.com/2023/05/01/crowd-control-interactive-stream/");
 
-const json = await fetch('https://yupdlekmfs.us12.qoddiapp.com/api/', {
+const response = await fetch('https://yupdlekmfs.us12.qoddiapp.com/api/', {
     method: 'POST',
-    mode: 'no-cors',
     body: data
 })
-.then(response => {
-    alert(Promise.resolve(response.json()));
-    // alert(response.json());
-})
-return json
+const jsonData = await response.json();
+return jsonData;
+
 }
